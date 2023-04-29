3a6effc09c6e36ce769381f46f18d55be025fc35
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-29 23:35:34 +0900

    feat: HTTP 요청 데이터(3) - API Message Body  - HTTP message body에 데이터를 직접 담아서 요청하는 단순 텍스트 출력

--- src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -18,6 +18,7 @@ public class RequestBodyStringServlet extends HttpServlet {
 
         // Message body 내용을 byte code로 바로 얻을 수 있다.
         ServletInputStream inputStream = request.getInputStream();
+
         // StreamUtils 클래스 사용
         // byte를 인코딩할 시 항상 인코딩 정보를 알려줘야 한다. 반대도 마찬가지
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
