2990cb9b37b47e1bc71e87e4e104c084509c239b
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-29 23:35:12 +0900

    feat: HTTP 요청 데이터(2) - API Message Body  - HTTP message body에 데이터를 직접 담아서 요청하는 단순 텍스트 출력

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Message body 내용을 byte code로 바로 얻을 수 있다.
+        ServletInputStream inputStream = request.getInputStream();
+        // StreamUtils 클래스 사용
+        // byte를 인코딩할 시 항상 인코딩 정보를 알려줘야 한다. 반대도 마찬가지
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+        response.getWriter().write("OK");
+    }
+}
