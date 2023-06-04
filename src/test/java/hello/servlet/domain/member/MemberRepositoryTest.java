d621838d1833cb551d3180778e252d97dfff0eb6
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-04 23:31:49 +0900

    feat: add MemberRepositoryTest - 0. class에 public 접근제어자 없어도 됨 - 1. assertj 사용 - 2. static import - 3. @Test 순서 보장 X -> @AfterEach clearStore 있는 이유

--- src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -1,4 +1,49 @@
 package hello.servlet.domain.member;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+// 0. public 없어도 됨
 class MemberRepositoryTest {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test // 3. @Test 순서 보장 X -> @AfterEach clearStore 있는 이유
+    void save() {
+        // given
+        Member member = new Member("hello", 20);
+
+        // when
+        Member savedMember = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember); // 1. assertj 사용
+    }
+
+    @Test
+    void findAll() {
+        // given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> result = memberRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2); // 2. static import
+        assertThat(result).contains(member1, member2);
+    }
 }
