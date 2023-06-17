9dd0e0e23e3c03a808dba967143048cfdf32ef60
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-17 23:21:21 +0900

    feat: FrontControllerServlet  - 다형성에 의해 적용되는 형식이다. 주석 참고

--- src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -13,11 +13,11 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-// urlPatterns = "/*" -> 하위에 있어도 무조건 이 경로대로 호출
+// urlPatterns = "~/*" -> ~ 포함 하위 모든 요청은 서블릿에서 받아들인다.
 @WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
 public class FrontControllerServletV1 extends HttpServlet {
 
-    private Map<String, ControllerV1> controllerMap = new HashMap<>();
+    private final Map<String, ControllerV1> controllerMap = new HashMap<>();
 
     public FrontControllerServletV1() {
         controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
@@ -28,5 +28,18 @@ public class FrontControllerServletV1 extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("FrontControllerServletV1.service");
+
+        // 요청한 url이 /front-controller/v1/members일 경우
+        // -> new MemberListControllerV1()와 같다.
+        String requestURI = request.getRequestURI();
+
+        // requestURI 조회 -> 실제 호출할 컨트롤러를 controllerMap에서 찾는다.
+        ControllerV1 controller = controllerMap.get(requestURI);
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND); // 없으면 404 Not Found
+            return;
+        }
+
+        controller.process(request, response);
     }
 }
