49cfc1f5b075f1722561d95ffe0fa797a791eb1d
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-18 23:07:14 +0900

    feat: modified Myview  - render model 값 받아오기  - 모델을 setAttribute로 바꿔야 해서 forEach 사용 -> modelToRequestAttribute()

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private final String viewPath;
@@ -17,5 +18,17 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 코드만 보면 잘 모르니까 메서드로 변경 -> modelToRequestAttribute()
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private static void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach(request::setAttribute);
+        // model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
